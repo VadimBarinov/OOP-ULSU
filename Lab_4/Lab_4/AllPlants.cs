ccfdd940d9c73c7da744645f749f397a355dd526
VadimBarinov <barinovvadim87@gmail.com>
2025-02-18 16:06:24 +0400

    Lab_4

---
+++ Lab_4/Lab_4/AllPlants.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    public abstract class AllPlants
+    {
+        public virtual void Print()
+        {
+            Console.WriteLine("Абстрактный метод");
+        }
+
+        public abstract string ToString();
+
+        public virtual string? GetColor(){ return null; }
+        public virtual double? GetSemLength(){ return null; }
+        public virtual string? GetName(){ return null; }
+
+        public static List<AllPlants> list = new List<AllPlants>();
+
+        public static void AddToList(AllPlants plant)
+        {
+            list.Add(plant);
+        }
+        public static void PrintListAllPlants()
+        {
+            Console.WriteLine("Список объектов:");
+            foreach (AllPlants plant in list)
+            {
+                Console.WriteLine(" - " + plant.ToString());
+            }
+        }
+
+        // количество роз заданного цвета
+        public static IEnumerable<int> GetColorRoseCount(string color)
+        {
+            int count = 0;
+
+            foreach (AllPlants plant in list)
+            {
+                if (plant is Rose && plant.GetColor() == color)
+                {
+                    count++;
+                }
+            }
+            yield return count;
+        }
+
+        // все чайные розы с длиной стебля большей, чем задал пользователь
+        public static IEnumerable<string> GetTeaRoseStemLength(double stemLength)
+        {   
+            foreach (AllPlants plant in list)
+            {
+                if (plant is Rose && plant.GetSemLength() > stemLength && plant.GetName() == "Чайная роза")
+                {
+                    yield return " - " + plant.ToString();   
+                }
+            }
+        }
+
+    }
+}
---
+++ Lab_4/Lab_4/Program.cs
@@ -0,0 +1,47 @@
+﻿// Баринов Вадим, МОАИС-0-22/1
+// Вариант 2
+
+namespace Lab_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+
+            // создаем экземпяры классов-наследников
+            Plant plant = new Plant("Пальма", "Дерево");
+            Wood wood = new Wood("Дуб", "Дерево", 12);
+            Flower flower = new Flower("Диантуз", "Цветок", 11.3);
+            Rose rose1 = new Rose("Роза кустовая", "Роза", 22.3, "Красная");
+            Rose rose2 = new Rose("Чайная роза", "Роза", 20.1, "Красная");
+            Rose rose3 = new Rose("Роза кустовая", "Роза", 10.0, "Белая");
+            Rose rose4 = new Rose("Чайная роза", "Роза", 25.2, "Розовая");
+            Rose rose5 = new Rose("Роза российская", "Роза", 12.7, "Красная");
+            Rose rose6 = new Rose("Чайная роза", "Роза", 14.6, "Красная");
+
+            // помещаем объекты в список
+            AllPlants.AddToList(plant);
+            AllPlants.AddToList(wood);
+            AllPlants.AddToList(flower);
+            AllPlants.AddToList(rose1);
+            AllPlants.AddToList(rose2);
+            AllPlants.AddToList(rose3);
+            AllPlants.AddToList(rose4);
+            AllPlants.AddToList(rose5);
+            AllPlants.AddToList(rose6);
+
+            // выводим список
+            AllPlants.PrintListAllPlants();
+
+            // вызываем фукнции 
+            Console.WriteLine("\nВсе чайные розы с длиной стебля большей, чем задал пользователь: ");
+            Console.WriteLine(string.Join("\n", AllPlants.GetTeaRoseStemLength(14.0)));
+
+            Console.WriteLine("\nКоличество роз заданного цвета: ");
+            Console.WriteLine(string.Join("\n", AllPlants.GetColorRoseCount("Красная")));
+
+            Console.ReadKey();
+        }
+    }
+}
