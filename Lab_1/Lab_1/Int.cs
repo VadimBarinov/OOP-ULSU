f52bdc2c732b1449e6adec836b7c2f057e606042
VadimBarinov <barinovvadim87@gmail.com>
2025-02-18 12:30:43 +0400

    init

---
+++ Lab_1/Lab_1/Int.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    class Int
+    {
+        private int value; 
+
+        public Int() 
+        {
+            value = 0;
+            Console.WriteLine("Вызван конструктор без параметров");
+        }
+        public Int(int value)
+        {
+            Console.WriteLine("Вызван конструктор с параметром");
+            this.value = value;
+        }
+        public Int(Int i)
+        {
+            Console.WriteLine("Вызван конструктор копирования");
+            this.value = i.value;
+        }
+        ~Int()
+        {
+            Console.WriteLine("Вызван деструктор");
+        }
+
+        public void SetValueNull() { this.value = 0; }
+
+        public void SetValue(int number) { this.value = number; }
+
+        public int GetValue() { return this.value; }
+
+        public void Print() { Console.WriteLine("Значение числа: {0}\n", this.value); }
+        public void SumValues(Int first, Int second) { this.value = first.value + second.value; }
+
+    }
+}
---
+++ Lab_1/Lab_1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Баринов Вадим, МОАИС-0-22/1
+// Вариант 2
+
+using System.Drawing;
+
+namespace Lab_1
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine();
+
+            // Общее задание
+            Int intValue1 = new Int();
+            intValue1.Print();
+
+            Int intValue2 = new Int(20);
+            intValue2.Print();
+
+            Int intValue3 = new Int(intValue2);
+            intValue3.Print();
+
+            // Индивидуальное задание
+            Int number1 = new Int();
+            number1.SetValue(33);
+            number1.Print();
+            Int number2 = new Int();
+            number2.SetValue(42);
+            number2.Print();
+            Int number3 = new Int();
+            number3.SumValues(number1, number2);
+            number3.Print();
+
+            Console.ReadKey();
+        }
+    }
+
+}
